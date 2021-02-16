1770c5e61ba874570ca92c4e897f622e40d650ee
McSteve7 <stephenajiboye@yahoo.com>
2021-02-16 09:15:19 +0100

    few changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
         mailchimp: true,
         authorsPage: true,
         sources: {
-          //local: true,
+          local: false,
           contentful: true,
         },
       },
